b9fa01d49d1999a1e6ed3605ea3dcd96655fd602
Jorge Junior <jorge13@gmail.com>
2026-06-24 01:33:57 -0300

    feat: inclui os desenhos de rabisco e retangulo na interface

--- etapa_02/poligonos/Retangulo.py
+++ etapa_02/poligonos/Retangulo.py
@@ -4,11 +4,13 @@ from etapa_02.poligonos.Figuras import Figuras
 class Retangulo(Figuras):
     def __init__(
         self,
-        nome,
-        x1,
-        y1,
-        x2,
-        y2,
+        canvas,
+        historico_figuras: list[tuple],
+        nome= None,
+        x1=0,
+        y1=0,
+        x2=0,
+        y2=0,
         cor_borda = 'black',
         cor_preenchimento = 'white'
         ):
@@ -21,39 +23,48 @@ class Retangulo(Figuras):
             cor_borda,
             cor_preenchimento
             )
+        self.canvas = canvas
+        self.historico_figuras = historico_figuras
+
+    def iniciar_figura(self,event):
+        self.figura_nova = (
+            'retangulo', 
+            (event.x, event.y,
+            event.x, event.y),
+            self.cor_borda, 
+            self.cor_preenchimento
+            )
 
 
-    def iniciar_figura(self,mouse_x,mouse_y):
-        self.x1 = mouse_x
-        self.y1 = mouse_y
-        self.x2 = mouse_x
-        self.y2 = mouse_y
-
-
-    def atualizar_figura(self, mouse_x, mouse_y):
-        self.x2 = mouse_x
-        self.y2 = mouse_y
+    def atualizar_figura(self, event):
+        self.figura_nova = (
+            'retangulo',
+            (self.figura_nova[1][0],
+            self.figura_nova[1][1], 
+            event.x, event.y), 
+            self.cor_borda, 
+            self.cor_preenchimento
+        )
         
     
-    def incluir_figura(self):
-        pass
+    def incluir_figura(self,event):
+        if not self.incompleta(self.figura_nova):
+            self.historico_figuras.append(self.figura_nova)
+        self.desenhar_figura()
     
-    def desenhar_figura(self,canvas,traco=False):
-        # Define se a linha do contorno será traco ou contínua
-        dash_para = (4,2) if traco else None
-        canvas.create_rectangle(
-            self.x1,
-            self.y1, 
-            self.x2, 
-            self.y2,
-            outline=self.cor_borda, 
-            fill=self.cor_preenchimento, 
-            dash=dash_para
-            )
+    def desenhar_figura(self):
+        return super().desenhar_figura()
         
     
     def desenhar_figura_nova(self):
-        pass
+        self.canvas.create_rectangle(
+            values,
+            dash=(4, 2),
+            outline=cor_outline,
+            fill=cor_fill ,
+            width=2
+            )
     
-    def incompleta(self):
-        return ((self.x1, self.y1) == (self.x2, self.y2))
+    def incompleta(self, event):
+        values = self.figura_nova[1]
+        return (values[0], values[1]) == (values[2], values[3])
