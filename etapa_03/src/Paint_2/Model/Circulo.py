73a679b17ab6f948987b3137c5c5840ab5c256f7
Jorge Junior <269608361+jorgejunior404@users.noreply.github.com>
2026-06-25 21:09:02 -0300

    refactor: refatorei os códigos de cada figura para que esteja no padrao de uso do controller

--- etapa_03/src/Paint_2/Model/Circulo.py
+++ etapa_03/src/Paint_2/Model/Circulo.py
@@ -4,77 +4,36 @@ from etapa_02.poligonos.Figuras import Figuras
 class Circulo(Figuras):
     def __init__(
         self,
-        canvas,
-        historico_figuras,
-        x1: int = 0,
-        y1: int = 0,
-        x2: int = 0,
-        y2: int = 0,
-        nome = None,
         cor_borda: str = 'black',
         cor_preenchimento: str = 'white',
         lados = 3
         ):
         super().__init__(
-            canvas,
-            historico_figuras,
-            x1,
-            y1,
-            x2,
-            y2,
-            nome,
             cor_borda,
             cor_preenchimento,
             lados
             )
+        
         self.raio = 0 
 
 
     def iniciar_figura(self, event):
-        self.figura_nova  = (
-            "circulo",
-            (event.x, event.y, event.x, event.y),
-            self.cor_borda,
-            self.cor_preenchimento
-        )
+        self.inic_x = event.x
+        self.inic_y = event.y
 
     def atualizar_figura(self, event):
         self.raio = min(
-        abs(event.x-self.figura_nova[1][0]),
-        abs(event.y-self.figura_nova[1][1])
+        abs(event.x-self.inic_x),
+        abs(event.y-self.inic_y)
         )
         self.figura_nova = (
             'circulo', 
-            (self.figura_nova[1][0], self.figura_nova[1][1], self.figura_nova[1][0]+self.raio, self.figura_nova[1][1]+self.raio),
+            (self.inic_x, self.inic_y, self.inic_x +self.raio, self.inic_y +self.raio),
             self.cor_borda,
             self.cor_preenchimento
             )
 
-        self.desenhar_figura()
-        self.desenhar_figura_nova()
       
-    
-    def incluir_figura(self,event):
-        if not self.incompleta(self.figura_nova): 
-            self.historico_figuras.append(self.figura_nova) 
-        self.desenhar_figura()
-
-    
-    def desenhar_figura(self):
-        return super().desenhar_figura()
-        
-    
-    def desenhar_figura_nova(self):
-        fig, values, cor_outline, cor_fill = self.figura_nova
-        self.canvas.create_oval(
-            values,
-            dash=(4, 2),
-            outline=cor_outline,
-            fill=cor_fill,
-            width=2
-            )
-        
-    
     def incompleta(self, event):
         values = self.figura_nova[1]
         return (values[0], values[1]) == (values[2], values[3])
--- etapa_03/src/Paint_2/Model/Linha.py
+++ etapa_03/src/Paint_2/Model/Linha.py
@@ -1,4 +1,4 @@
-from etapa_02.poligonos.Figuras import Figuras
+from etapa_03.src.Paint_2.Model import Figuras
 #from tkinter import *
 
 #Todos os comentarios aqui são para a interface
@@ -7,65 +7,29 @@ from etapa_02.poligonos.Figuras import Figuras
 class Linha(Figuras):
     def __init__(
         self,
-        canvas,
-        historico_figuras: list[tuple],
-        nome = None,
-        x1: int = 0,
-        y1: int = 0,
-        x2: int = 0,
-        y2: int = 0,
         cor_borda: str = 'black',
         cor_preenchimento: str = 'white',
         lados = 3
         ):
         super().__init__(
-            nome,
-            x1,
-            y1,
-            x2,
-            y2,
             cor_borda,
             cor_preenchimento,
             lados
             )
-        self.canvas = canvas
-        self.historico_figuras = historico_figuras
 
 
-    def iniciar_figura(self,event):
-        self.figura_nova  = (
-            "linha",
-            (event.x, event.y, event.x, event.y),
-            self.cor_borda,
-            self.cor_preenchimento
-        )
+    def iniciar_figura(self, event):
+        self.inic_x = event.x
+        self.inic_y = event.y
 
         
     def atualizar_figura(self,event):
         self.figura_nova = (
             "linha",
-            (self.figura_nova[1][0], self.figura_nova[1][1], event.x, event.y),
+            (self.inic_x, self.inic_y, event.x, event.y),
             self.cor_borda,
             self.cor_preenchimento
             )
-        
-        self.desenhar_figura()
-        self.desenhar_figura_nova()
-
-    def desenhar_figura(self):
-        return super().desenhar_figura()
-        
-    
-    def desenhar_figura_nova(self):
-        values = self.figura_nova[1]
-        self.canvas.create_line(values[0], values[1], values[2], values[3], dash=(4, 2))
-        
-
-    def incluir_figura(self,event):
-        if not self.incompleta(self.figura_nova): 
-            self.historico_figuras.append(self.figura_nova) 
-        self.desenhar_figura()
-    
     
     def incompleta(self, event):
         values = self.figura_nova[1]
